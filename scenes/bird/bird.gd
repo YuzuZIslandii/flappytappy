59fa89bae9b61f324c6a0913d9458cd732e4ee8e
“YuzuZIslandii” <yuzuzislandii@hotmail.com>
2024-08-18 09:47:57 +0000

    fixing first commit

---
+++ scenes/bird/bird.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+signal on_bird_died
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
+
+
+const GRAVITY: float = 1500.0
+const POWER: float = -500.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+#fizyka ptaka
+func _physics_process(delta):
+	fly(delta)
+	move_and_slide()
+	if is_on_floor() == true:
+		die()
+
+
+
+func fly(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
+		animation_player.play("power")
+
+
+func die() -> void:
+	set_physics_process(false)
+	animated_sprite_2d.stop()
+	on_bird_died.emit()
---
+++ scenes/pipes/pipes.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x -= delta * 120.0
+
+
+func _on_screen_exited():
+	queue_free()
+	
+	
+func _on_laser_body_exited(body):
+	if body.is_in_group("player") == true:
+		print("Point Scored")
+
+
+func _on_pipe_body_entered(body):
+	if body.is_in_group("player") == true:
+		if body.has_method("die") == true:
+			body.die()
